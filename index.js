5bb076056e9d6bcdb4d26314a71b8d49b0fe8450
Nomera67 <94490777+Nomera67@users.noreply.github.com>
2024-04-10 16:37:28 +0200

    Connexion to MongoDB Atlas with mongoose and add models for book

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(express.json());
-
 const port = process.env.PORT || 3000;
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
